559e6c9ae9a58369bee0eadca59f123feeb08c7c
Rhythm Chopra <rhythm.chopra@tonboimaging.com>
2018-10-03 19:26:01 +0530

    added qemu-user-static to do_init and cleanup updated.

--- cleanup.sh
+++ cleanup.sh
@@ -7,11 +7,13 @@ fi
 
 set -eu pipefail
 
-echo "Removing kali sources from sources.list"
-sudo sed -i '$d' /etc/apt/sources.list
-sudo sed -i '$d' /etc/apt/sources.list
+if grep -q "deb http://http.kali.org/kali kali-rolling main non-free contrib" "/etc/apt/sources.list"; then
+	echo "Removing kali sources from sources.list"
+	sudo sed -i '$d' /etc/apt/sources.list
+	sudo sed -i '$d' /etc/apt/sources.list
+fi
 echo "Removing kali keyring ..."
-apt purge -y kali-archive-keyring debootstrap
+apt purge -y kali-archive-keyring debootstrap qemu-user-static
 
 echo "Cleaning Up ..."
 apt -y autoremove
--- do_init.sh
+++ do_init.sh
@@ -5,7 +5,10 @@ if [ $EUID -ne 0 ];then
 	exit 1
 fi
 
+basedir=$(pwd)
+
 set -eu pipefail
+
 if ! grep -q "deb http://http.kali.org/kali kali-rolling main non-free contrib" "/etc/apt/sources.list"; then
 	echo "Adding kali sources to sources.list ..."
 	echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
@@ -15,14 +18,15 @@ fi
 echo "Adding kali archive gpg signatures ..."
 wget -q -O - https://archive.kali.org/archive-key.asc | apt-key add
 
-echo "Installing kali archive keyrings and debootstrap ..."
-apt update
-apt install -y kali-archive-keyring debootstrap
+echo "Installing required packages ..."
+apt update || apt -y -f install
+apt install -y kali-archive-keyring debootstrap qemu-user-static
 
 echo "Adding kali-rolling to debootstrap scripts ..."
 cd '/usr/share/debootstrap/scripts/'
 (echo "default_mirror http://http.kali.org/kali"; sed -e "s/debian-archive-keyring.gpg/kali-archive-keyring.gpg/g" sid) > kali
 ln -sf kali kali-rolling
+cd ${basedir}
 
 # You can here change if you want some customized kernel
 echo "Cloning linux kernel 4.18..."
@@ -31,7 +35,7 @@ git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git -b linu
 
 # You can change if you want to use customized kernel firmware
 echo "Cloning linux kernel firmware ..."
-git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git linux-firmware
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git linux-firmware
 
 # You can change here if you want some customized bootloader
 echo "Cloning u-boot ..."
